5d6fb9b29eb0e35d4d6da8fca346f8f03bf03d84
Guilherme <69487263+GuiAlcantaraa@users.noreply.github.com>
2021-11-29 20:13:42 -0300

    Exercicios att

--- Exercicios.sql
+++ Exercicios.sql
@@ -1,4 +1,5 @@
-		
+
+	
 	// EXERCICIO 1 //
 
 	SELECT * FROM Funcionario
@@ -38,33 +39,132 @@
 
 	// EXERCICIO 10 //
 
-	select * from TrabalhaProjeto
-
 	select 
-	min(QtdeHoras) menor_hora
-	FROM TrabalhaProjeto
+	Projeto.Nome,
+	Min(TrabalhaProjeto.QtdeHoras)	
+	from TrabalhaProjeto
+	inner join Projeto on Projeto.ProjetoID = TrabalhaProjeto.ProjetoID
+	group by Projeto.Nome
 
-	
 	// EXERCICIO 11 //
 
-	SELECT 
-	PROJETOID,
-	AVG(QtdeHoras)
-	FROM TrabalhaProjeto
-	GROUP BY ProjetoID
+	select 
+	Projeto.Nome,
+	AVG(TrabalhaProjeto.QtdeHoras)
+	from TrabalhaProjeto
+	inner join Projeto on Projeto.ProjetoID = TrabalhaProjeto.ProjetoID
+	group by Projeto.Nome
+
 
 	// EXERCICIO 12 //
 
+	select sexo, count(FuncionarioID) as Quantidade
+	from Funcionario
+	group by sexo
+
 	// EXERCICIO 13 //
+
+	select top 2
+	Nome,
+	DataNascimento
+	from Funcionario order by DataNascimento
 
 	// EXERCICIO 14 //
 
 	// EXERCICIO 15 //
-	
-	SELECT 
-	DEPENDENTE.DependenteID,
-	DEPENDENTE.Nome
-	FROM Dependente
-	inner join funcionario 
-	on Dependente.DependenteId = FUNCIONARIO.FUNCIONARIOID
 
+	// EXERCICIO 16 //
+
+
+
+	// EXERCICIO 17 //
+
+	select*from Projeto
+	INNER JOIN TrabalhaProjeto
+	ON TrabalhaProjeto.ProjetoID = Projeto.ProjetoID
+	INNER JOIN Funcionario
+	ON TrabalhaProjeto.FuncionarioID = Funcionario.FuncionarioID
+
+
+	// EXERCICIO 18 //
+
+	select *from Funcionario
+	left join Dependente
+	on Funcionario.FuncionarioID = Dependente.FuncionarioID
+	where Dependente.DependenteID is null
+
+	// EXERCICIO 19 //
+
+	select 
+	Funcionario.Nome,
+	count (Dependente.DependenteID) QuantidadeDeDependentes
+	from Funcionario
+	inner join Dependente
+	on Funcionario.FuncionarioID = Dependente.FuncionarioID
+	group by Funcionario.Nome
+
+	// EXERCICIO 20 //
+
+	select 
+	Funcionario.Nome,
+	ISNULL(sum(TrabalhaProjeto.QtdeHoras), 0) HorasTrabalhadas
+	from  Funcionario
+	left join TrabalhaProjeto
+	on Funcionario.FuncionarioID = TrabalhaProjeto.FuncionarioID
+	group by Funcionario.Nome
+
+
+	// EXERCICIO 25 //
+
+		select Month(DataNascimento) Mes,
+				count(FuncionarioID) QTDE
+		from Funcionario
+		group by Month(DataNascimento) 
+		order by Mes
+
+
+	// EXERCICIO 26 //
+
+	select 
+		Year(DataNascimento) Ano,
+		Month(DataNascimento) Mes,
+		count(FuncionarioID) QTDE
+	from Funcionario
+	group by Month(DataNascimento), Year(DataNascimento)
+	order by Ano
+
+
+	// EXERCICIO 27 //
+
+	select Nome,
+			DataNascimento,
+			DATEDIFF(month, DataNascimento, getdate()) / 12
+	FROM Funcionario
+
+
+	// EXERCICIO 28 //
+
+	create view VwFuncionarios
+
+	as 
+	select Nome,
+			FuncionarioID,
+			DataNascimento
+	from Funcionario
+
+	select * from VwFuncionarios
+
+
+
+
+
+
+
+
+
+
+
+
+
+	
+	
